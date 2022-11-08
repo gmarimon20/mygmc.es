26d8dd6eb9f598711d83e96167d94f2ec7212443
gmarimon <73643558+gmarimon20@users.noreply.github.com>
2022-11-08 11:47:39 +0100

    Delete src directory

--- src/main/java/net/gerard/web/repository/SolicitudesRepository.java
+++
@@ -1,7 +0,0 @@
-package net.gerard.web.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import net.gerard.web.model.Solicitud;
-
-public interface SolicitudesRepository extends JpaRepository<Solicitud, Integer> {
-}
--- src/main/java/net/gerard/web/repository/UsuariosRepository.java
+++
@@ -1,9 +0,0 @@
-package net.gerard.web.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.gerard.web.model.Usuario;
-
-public interface UsuariosRepository extends JpaRepository<Usuario, Integer> {
-	Usuario findByUsername(String username);
-}
