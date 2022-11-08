4e549aa2dfd79e0a8b59d25e5b49b090c5eec6ff
gmarimon <73643558+gmarimon20@users.noreply.github.com>
2022-11-08 12:02:43 +0100

    Delete web directory

--- web/src/main/java/net/gerard/web/repository/PerfilesRepository.java
+++
@@ -1,8 +0,0 @@
-package net.gerard.web.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.gerard.web.model.Perfil;
-
-public interface PerfilesRepository extends JpaRepository<Perfil, Integer> {
-}
